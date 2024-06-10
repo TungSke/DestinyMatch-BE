482244cc9868939818582800899c96e008638809
TrinhSonTung <trinhsontung2410@gmail.com>
2024-06-10 17:03:55 +0700

    update connection string and swagger when deploy

--- FPT.DestinyMatch/FPT.DestinyMatch.API/Program.cs
+++ FPT.DestinyMatch/FPT.DestinyMatch.API/Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
