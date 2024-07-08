106c4ebb4fab10d5f8e5634275a72cc85355c277
TrinhSonTung <trinhsontung2410@gmail.com>
2024-07-08 21:43:05 +0700

    update send message real time

--- FPT.DestinyMatch/FPT.DestinyMatch.API/ChatHub.cs
+++ FPT.DestinyMatch/FPT.DestinyMatch.API/ChatHub.cs
@@ -6,9 +6,38 @@ namespace FPT.DestinyMatch.API
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(Guid conversationId, string message)
+        public async Task SendMessage(string matchId, string memberId, string message)
         {
-            await Clients.Client(conversationId.ToString()).SendAsync("ReceiveMessage", conversationId, message);
-        }      
+            Console.WriteLine($"Message sent from {memberId} to ${matchId} with content ${message}");
+            try
+            {
+                await Clients.Group(matchId).SendAsync("ReceiveMessage", memberId, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending message: {ex.Message}");
+                throw;
+            }
+        }
+
+
+        public async Task JoinGroup(string memberId, string matchId)
+        {
+            try
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, matchId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error joining group: {ex.Message}");
+                throw;
+            }
+        }
+
+
+        public async Task LeaveGroup(string matchId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, matchId);
+        }
     }
 }
